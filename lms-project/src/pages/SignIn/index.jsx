3c75949eecad7f5d8f91e53fa02013deb3f8e766
ree <hannn9491@gmail.com>
2024-09-15 19:34:57 +0700

    manage Frontend SignUp

--- lms-project/src/pages/SignIn/index.jsx
+++ lms-project/src/pages/SignIn/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Navbar from '../../components/navbar'
 
 export default function SignInPage() {
     return (
@@ -8,23 +9,7 @@ export default function SignInPage() {
             <img src="/assets/images/backgrounds/background-glow.png" className="absolute bottom-0 transform -translate-x-1/2 left-1/2" alt="" />
         </div>
         <nav className="flex items-center justify-between p-[30px]">
-            <div className="flex items-center gap-[60px]">
-                <img src="/assets/images/logos/logo.svg" className="flex shrink-0" alt="logo" />
-                <ul className="flex items-center gap-10">
-                    <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-                        <Link to="#">Home</Link>
-                    </li>
-                    <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-                        <Link to="#">Pricing</Link>
-                    </li>
-                    <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-                        <Link to="#">Features</Link>
-                    </li>
-                    <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-                        <Link to="#">Testimonials</Link>
-                    </li>
-                </ul>
-            </div>
+            <Navbar />
             <div className="flex items-center gap-3">
                 <Link to="#" >
                     <div className="flex items-center gap-3 w-fit rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#070B24] border-[#24283E] shadow-[-10px_-6px_10px_0_#181A35_inset]">
