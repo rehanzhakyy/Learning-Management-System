3a0019062f548c4c0b2380e8a7489fa2800c6b5d
ree <hannn9491@gmail.com>
2024-09-15 19:10:44 +0700

    manage Frontend - Sign In

--- lms-project/src/router/index.jsx
+++ lms-project/src/router/index.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import ManagerHome from "../pages/ManagerHome";
+import SignInPage from "../pages/SignIn";
 
-const router = createBrowserRouter ([{
+const router = createBrowserRouter ([
+    {
     path: "/",
     element: <ManagerHome />
-}])
+    },
+    {
+    path: "/manager/sign-in",
+    element: <SignInPage />
+    }
+])
 
 export default router
